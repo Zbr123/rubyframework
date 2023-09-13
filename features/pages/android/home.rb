5c5b7aa8118f17138ada0c9931eb0390b82f1741
Syed Zubair Alam <syedzubairalam123@gmail.com>
2023-09-13 15:41:03 +0500

    push

---
+++ features/pages/android/home.rb
@@ -0,0 +1,17 @@
+class Home
+  include Helper
+
+  def email_textfield
+    return driver.find_element(:xpath, "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.EditText")
+  end
+
+  def sign_in_btn
+    return driver.find_element(:xpath, "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView")
+  end
+
+  def Login(email_or_phone)
+    email_textfield.send_keys(email_or_phone)
+    click_on_element(sign_in_btn)
+  end
+
+end
---
+++ features/step_definitions/android/android_dash_steps.rb
@@ -0,0 +1,7 @@
+Given("App's login page is opened") do
+  home = Home.new
+  home.Login("szubair.alam@toptal.com")
+end
+
+Then("Orders are present on the Orders tab") do
+end
